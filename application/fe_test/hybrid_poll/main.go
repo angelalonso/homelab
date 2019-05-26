ec71993088dce51b6ae0fe80abb430a274bf485d
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-05-26 21:01:07 +0200

    working poll on js and golang

--- application/fe_test/hybrid_poll/main.go
+++ application/fe_test/hybrid_poll/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -15,9 +18,49 @@ func Router() *mux.Router {
 
 func CreateEndpoint(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
-	w.Write([]byte("Item Created"))
+	w.Write([]byte(CreateContent()))
+}
+
+func CreateContent() string {
+	content := []byte{}
+	html_header, err := ioutil.ReadFile("static/html_header")
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		os.Exit(1)
+	}
+	css, err_css := ioutil.ReadFile("static/css")
+	if err_css != nil {
+		fmt.Println("Error: " + err_css.Error())
+		os.Exit(1)
+	}
+	html_main, err_html_main := ioutil.ReadFile("static/html_main")
+	if err_html_main != nil {
+		fmt.Println("Error: " + err_html_main.Error())
+		os.Exit(1)
+	}
+	js, err_js := ioutil.ReadFile("static/js")
+	if err_js != nil {
+		fmt.Println("Error: " + err_js.Error())
+		os.Exit(1)
+	}
+	html_footer, err_html_footer := ioutil.ReadFile("static/html_footer")
+	if err_html_footer != nil {
+		fmt.Println("Error: " + err_html_footer.Error())
+		os.Exit(1)
+	}
+
+	content = append(content, html_header...)
+	content = append(content, css...)
+	content = append(content, html_main...)
+	content = append(content, js...)
+	content = append(content, html_footer...)
+
+	return string(content)
+
 }
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", Router()))
+	PORT := "4480"
+	fmt.Println("Serving on port " + PORT + "...")
+	log.Fatal(http.ListenAndServe(":"+PORT, Router()))
 }
--- application/fe_test/hybrid_poll/main_test.go
+++ application/fe_test/hybrid_poll/main_test.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateEndpoint(t *testing.T) {
+	request, err := http.NewRequest("GET", "/", nil)
+	assert.Equal(t, nil, err, "nil Error on getting request is expected")
+	response := httptest.NewRecorder()
+	Router().ServeHTTP(response, request)
+
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+}
+
+func TestCreateContent(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/", nil)
 	response := httptest.NewRecorder()
 	Router().ServeHTTP(response, request)
-	assert.Equal(t, 200, response.Code, "OK response is expected")
+	// the page Content must be readable
+	pageContentInBytes, err := ioutil.ReadAll(response.Body)
+	assert.Equal(t, nil, err, "nil Error on reading content buffer is expected")
+	// the result of CreateContent must not be empty
+	//assert.Equal(t, CreateContent(), pageContent, "OK response is expected")
+	// the page content must be the same as the result of CreateContent
+	pageContent := string(pageContentInBytes)
+	assert.Equal(t, CreateContent(), pageContent, "OK response is expected")
+	// the page must have a title
+	titleStartIndex := strings.Index(pageContent, "<!DOCTYPE html>")
+	assert.NotEqual(t, -1, titleStartIndex, "There should be a DOCTYPE tag")
+
 }
