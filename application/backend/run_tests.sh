367ce6906ed67ca3e7e3518b9c4971d66bdf8658
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-07-07 15:53:23 +0200

    adding go get for backend tests

--- application/backend/run_tests.sh
+++ application/backend/run_tests.sh
@@ -2,6 +2,8 @@
 PWD=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
+go get -u github.com/gorilla/mux
+go get -u github.com/lib/pq
 go test -v -cover
 RESULT=$?
 cd $PWD
