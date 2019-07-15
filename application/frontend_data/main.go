e677f79c64da29c5f7b4a6bd53ccfe5f2908f7e2
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-07-15 23:15:01 +0200

    adding a functional joke box

--- application/frontend_data/main.go
+++ application/frontend_data/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/romana/rlog"
 )
 
 type Dependency struct {
@@ -22,21 +25,46 @@ type Check struct {
 	Dependencies []Dependency `json:"dependencies"`
 }
 
+type Post struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var posts []Post
+
 func Router() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", CreateMain).Methods("GET")
 	router.HandleFunc("/check", CreateCheck).Methods("GET")
+	router.Path("/post/").Methods("GET").HandlerFunc(CreateResult)
 	return router
 }
 
 func CreateMain(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
-	w.Write([]byte("ok"))
+	w.Write([]byte(CreateMainContent()))
 }
 
 func CreateCheck(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte(CreateCheckContent()))
+}
+
+func CreateResult(w http.ResponseWriter, req *http.Request) {
+	joke, _ := req.URL.Query()["joke"]
+	fmt.Println(joke)
+	w.WriteHeader(200)
+	w.Write([]byte(strings.Join(joke, " ")))
+}
+
+func CreateMainContent() string {
+	html_main, err_html_main := ioutil.ReadFile("static/html_main")
+	if err_html_main != nil {
+		rlog.Error("Error reading html_main: " + err_html_main.Error())
+		os.Exit(1)
+	}
+	return FormatContent(string(html_main))
 }
 
 func CreateCheckContent() string {
@@ -75,6 +103,38 @@ func TestConnection(url string) (response string) {
 	}
 
 	return response
+}
+
+func FormatContent(mainContent string) string {
+	content := []byte{}
+	html_header, err := ioutil.ReadFile("static/html_header")
+	if err != nil {
+		rlog.Error("Error reading html_header: " + err.Error())
+		os.Exit(1)
+	}
+	css, err_css := ioutil.ReadFile("static/css")
+	if err_css != nil {
+		rlog.Error("Error reading css: " + err_css.Error())
+		os.Exit(1)
+	}
+	js, err_js := ioutil.ReadFile("static/js")
+	if err_js != nil {
+		rlog.Error("Error reading js: " + err_js.Error())
+		os.Exit(1)
+	}
+	html_footer, err_html_footer := ioutil.ReadFile("static/html_footer")
+	if err_html_footer != nil {
+		rlog.Error("Error reading html_footer: " + err_html_footer.Error())
+		os.Exit(1)
+	}
+
+	content = append(content, html_header...)
+	content = append(content, css...)
+	content = append(content, mainContent...)
+	content = append(content, js...)
+	content = append(content, html_footer...)
+
+	return string(content)
 }
 
 func main() {
