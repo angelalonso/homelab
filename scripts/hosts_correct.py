48c9b47a80bd3bcfbfebc3c24c6941c666bee453
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-07-31 23:42:04 +0200

    getting etchosts corrected

--- scripts/hosts_correct.py
+++ scripts/hosts_correct.py
@@ -12,15 +12,32 @@ HOSTS = [
         "tokyo"
         ]
 
-def get_host():
-    for ip_end in range(1, 255):
-        ip = "192.168.0." + str(ip_end)
-        try:
-            entry = socket.gethostbyaddr(ip)[0]
-            if entry in HOSTS:
-                print(entry + " " + ip)
-        except socket.herror:
-            pass
+def sed_etchosts():
+    with open("/etc/hosts", "r") as sources:
+        lines = sources.readlines()
+    for line in lines:
+        for host in HOSTS:
+            if host in line:
+                newline = get_ip(host, "192.168.0", 255)
+            else:
+                newline = line
+        print(newline)
+
+def get_ip(hostname, base_ip, range_ips):
+    for ip_end in range(1, range_ips):
+        ip = base_ip + "." + str(ip_end)
+        if ip != None:
+            return get_hostname(ip)
+
+def get_hostname(ip):
+    try:
+        entry = socket.gethostbyaddr(ip)[0]
+        if entry in HOSTS:
+            return entry + " " + ip
+    except socket.herror:
+        pass
+    except socket.gaierror:
+        print("Error " + ip)
 
 if __name__ == "__main__":
-    get_host()
+    sed_etchosts()
