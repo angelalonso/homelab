1f229b6a607c4f9bebd24c654c39db3b670945c3
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-08-01 17:23:28 +0200

    script getting proper etchosts

--- scripts/hosts_correct.py
+++ scripts/hosts_correct.py
@@ -13,31 +13,35 @@ HOSTS = [
         ]
 
 def sed_etchosts():
+    newline = ""
     with open("/etc/hosts", "r") as sources:
         lines = sources.readlines()
     for line in lines:
         for host in HOSTS:
             if host in line:
                 newline = get_ip(host, "192.168.0", 255)
+                break
             else:
-                newline = line
+                newline = line.rstrip('\n')
         print(newline)
 
 def get_ip(hostname, base_ip, range_ips):
     for ip_end in range(1, range_ips):
         ip = base_ip + "." + str(ip_end)
-        if ip != None:
-            return get_hostname(ip)
+        hostresult = get_hostname(hostname, ip)
+        if hostresult != None:
+            return hostresult
 
-def get_hostname(ip):
+def get_hostname(hostname, ip):
     try:
         entry = socket.gethostbyaddr(ip)[0]
-        if entry in HOSTS:
-            return entry + " " + ip
+        if entry == hostname:
+            return ip + " " + entry 
     except socket.herror:
         pass
     except socket.gaierror:
-        print("Error " + ip)
+        pass
 
 if __name__ == "__main__":
     sed_etchosts()
+    #print(get_ip("tokyo", "192.168.0", 255))
