227e745b2b21769c1228d17ba3c4595883dc2c57
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-08-11 15:57:16 +0200

    checking that there is indeed a function like the one expected

--- scripts/d8s/d8s.py
+++ scripts/d8s/d8s.py
@@ -4,11 +4,14 @@ Use case:
     This program is meant to help manage docker swarm, wrapping some commands up
 """
 import sys
-import ctl_params
+import paramctl
+#import ctl_params
+import sh
 
 
 def get_nodes(*argv):
     args = " "
+    print(sh.kubectl("--kubeconfig", "/home/aaf/.kube/config.testing", "get", "no"))
     for arg in argv:
         args = args.join(arg)
     return "you got this far " + args
@@ -20,8 +23,12 @@ if __name__ == '__main__':
       and parameters, according to the parametermap loaded,
       then calls the corresponding function
     """
-    params = ctl_params.ParameterMap("parametermap.json")
+    #params = ctl_params.ParameterMap("parametermap.json")
+    params = paramctl.ParameterMap("parametermap.json")
 
     # This builds  and runs the call to a function dinamically
     function = params.check_args_input(sys.argv)
-    print(globals()[function[0]](function[1:]))
+    try:
+        print(globals()[function[0]](function[1:]))
+    except KeyError:
+        print("No function available like " + " ".join(function[:]))
