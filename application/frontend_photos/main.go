7b144ffb6cd45c986ece857167d01329f07629c0
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-09-29 15:33:39 +0200

    templating folder structure for images

--- application/frontend_photos/main.go
+++ application/frontend_photos/main.go
@@ -8,14 +8,23 @@ import (
 )
 
 type WebData struct {
-	Title string
+	Title  string
+	Image1 string
+	Image2 string
+	Image3 string
+	Image4 string
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	image_folder := "/static/"
 	tmpl, _ := template.ParseFiles("templates/layout.html", "templates/home.html")
 	now := time.Now()
 	wd := WebData{
-		Title: now.String(),
+		Title:  now.String(),
+		Image1: image_folder + "mountain.jpg",
+		Image2: image_folder + "forrest.jpg",
+		Image3: image_folder + "coffee.jpg",
+		Image4: image_folder + "beach.jpg",
 	}
 	if err := tmpl.Execute(w, &wd); err != nil {
 		log.Println(err.Error())
