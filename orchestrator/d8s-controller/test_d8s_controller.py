5210442e2b25c3c906d4f5823d87d5d378eea205
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-10-14 22:19:09 +0200

    getting swarm status

--- orchestrator/d8s-controller/test_d8s_controller.py
+++ orchestrator/d8s-controller/test_d8s_controller.py
@@ -6,5 +6,8 @@ class HelloworldTests(unittest.TestCase):
     def test_get_helloworld(self):
         self.assertEqual(d8sc.get_greetings(), 'Hello World!')
 
+    def test_get_docker_swarm_status(self):
+        self.assertEqual(d8sc.get_docker_swarm_status(), [])
+
 if __name__ == '__main__':
     unittest.main()
