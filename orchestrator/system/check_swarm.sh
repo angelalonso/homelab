ece5c12d3a81217a0f6450b97a6abc5d53096a61
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-10-15 21:09:37 +0200

    removing hardcoded keys

--- orchestrator/system/check_swarm.sh
+++ orchestrator/system/check_swarm.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #set -x
+
 USER=$1
 SSHKEY=$2
+
 MASTERIP="192.168.0.15"
 NODES="lisboa praha dublin"
-#NODES="lisboa"
+#NODES="dublin"
 DB_NODE="dublin"
 
 function check_master_ip {
@@ -17,23 +19,18 @@ function check_master_ip {
 function check_nodes {
   JOINCMD=$(docker swarm join-token worker | grep join)
   for node in $NODES; do 
-    #echo $node
+    echo checking $node
     STATUS=$(docker node ls | grep $node | awk '{print $3 $4}')
-    if [[ $"STATUS" != "ReadyActive" ]]; then
+    if [[ $STATUS != "ReadyActive" ]]; then
       echo "Leaving the swarm..."
-
-      ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p 21012 "hostname"
-      echo "ssh exit: "$?
       ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p 21012 "docker swarm leave"
-      echo "ssh exit: "$?
       sleep 5
       docker node rm $node
       sleep 5
       ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p 21012 "$JOINCMD"
     fi
-    echo $STATUS
   done
 }
-#date "+%Y%m%d-%T"
+date "+%Y%m%d-%T"
 check_master_ip
 check_nodes
