f0b57c70d3f60bb34963003da429ee2c80628a7a
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-10-15 20:51:34 +0200

    working restart of nodes

--- orchestrator/system/check_swarm.sh
+++ orchestrator/system/check_swarm.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-
+#set -x
+USER=$1
+SSHKEY=$2
 MASTERIP="192.168.0.15"
 NODES="lisboa praha dublin"
+#NODES="lisboa"
 DB_NODE="dublin"
 
 function check_master_ip {
-  IP_OK=$(ifconfig | grep $MASTERIP)
-  if [[ $IP_OK == "" ]]; then
+  IP_OK=$(/sbin/ifconfig | grep $MASTERIP)
+  if [[ $"IP_OK" == "" ]]; then
     echo "wrong IP"
   fi
 }
@@ -14,20 +17,23 @@ function check_master_ip {
 function check_nodes {
   JOINCMD=$(docker swarm join-token worker | grep join)
   for node in $NODES; do 
-    echo $node
+    #echo $node
     STATUS=$(docker node ls | grep $node | awk '{print $3 $4}')
-    if [[ $STATUS != "ReadyxActive" ]]; then
+    if [[ $"STATUS" != "ReadyActive" ]]; then
       echo "Leaving the swarm..."
-      ssh aafmin@$node -p 21012 "docker swarm leave"
+
+      ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p 21012 "hostname"
+      echo "ssh exit: "$?
+      ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p 21012 "docker swarm leave"
+      echo "ssh exit: "$?
       sleep 5
-      echo "Removing node $node"
       docker node rm $node
-      echo "Node $node is now joining the swarm"
-      ssh aafmin@$node -p 21012 "$JOINCMD"
+      sleep 5
+      ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p 21012 "$JOINCMD"
     fi
     echo $STATUS
   done
 }
-
+#date "+%Y%m%d-%T"
 check_master_ip
 check_nodes
