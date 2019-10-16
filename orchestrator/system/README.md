6a4803d86cba612005c221cb9c790f7ce841f04c
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-10-16 20:42:30 +0200

    typo on TESTLABEL and new echoes

--- orchestrator/system/README.md
+++ orchestrator/system/README.md
@@ -5,11 +5,8 @@
 Checks all nodes and removes/rejoins any node that might be failing.  
 
 ### Installation
-Add the following to your crontab: 
-```*/10 * * * * <INSTALLDIR>/orchestrator/system/check_swarm.sh <SSHUSER> <SSHPORT> <PATHTOSSHKEY> > <INSTALLDIR>/orchestrator/system/lastrun.log 2>&1```
-
-, where  
-INSTALLDIR is the full path to where you installed the homelab repo  
-SSHUSER is the user to SSH into the nodes  
-SSHPORT is the port to SSH into the nodes  
-PATHTOSSHKEY is the full path to the private key for the SSHUSER  
+Copy .env.dist to .env and adapt the values of the variables.  
+Add the following to your crontab:  
+```*/10 * * * * <INSTALLDIR>/orchestrator/system/check_swarm.sh > <INSTALLDIR>/orchestrator/system/lastrun.log 2>&1```  
+  
+, where INSTALLDIR is the full path to where you installed the homelab repo.
--- orchestrator/system/check_swarm.sh
+++ orchestrator/system/check_swarm.sh
@@ -2,11 +2,14 @@
 #set -x
 
 function get_vars {
+  echo "- Loading vars..."
   SELFPATH="$( cd "$(dirname "$0")" ; pwd -P )"
   source $SELFPATH/.env
+  echo "...loaded."
 }
 
 function test_vars {
+  echo "- Testing vars are correct..."
   if [[ -z $USER || \
     -z $PORT || \
     -z $SSHKEY || \
@@ -17,41 +20,49 @@ function test_vars {
     echo 'ERROR: one or more variables are undefined, Check/Create your .env file'
     exit 1
   fi
+  echo "... vars are correct."
 }
 
 function check_master_ip {
+  echo "- Checking the Master IP didn't change..."
   IP_OK=$(/sbin/ifconfig | grep $MASTERIP)
   if [[ $"IP_OK" == "" ]]; then
-    echo "wrong IP"
+    echo "...it changed!"
+  else
+    echo "...it didn't change."
   fi
 }
 
 function correct_nodes {
+  echo "- Checking that all nodes are OK..."
   JOINCMD=$(docker swarm join-token worker | grep join)
   for node in $NODES; do 
     echo checking $node
     STATUS=$(docker node ls | grep $node | awk '{print $3 $4}')
     if [[ $STATUS != "ReadyActive" ]]; then
-      echo "node $node leaving the swarm..."
+      echo "...node $node is not OK!\n, forcing it to leave the swarm..."
       ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p $PORT "docker swarm leave"
       sleep 5
-      echo "Removing node $node from the swarm..."
+      echo ", removing node $node from the swarm..."
       docker node rm $node
       sleep 5
-      echo "Rejoining node $node to the swarm..."
+      echo ", rejoining node $node to the swarm..."
       ssh -i $SSHKEY -o "StrictHostKeyChecking no" $USER@$node -p $PORT "$JOINCMD"
     else
-      echo $node is $STATUS
+      echo "...node $node is $STATUS."
     fi
   done
 }
 
 function correct_nodes_db {
+  echo "- Checking that all DB nodes are labelled correctly..."
   for node in $NODES_DB; do
     TESTLABEL=$(docker node inspect $node | grep type)
-    if [[ "$TEST" != '                "type": "db"' ]]; then
-      echo "Adding the db label to node $node"
+    if [[ "$TESTLABEL" != '                "type": "db"' ]]; then
+      echo "...node $node is not!\n, adding the db label to node $node"
       docker node update --label-add type=db $node
+    else
+      echo "...node $node is already labelled a db one."
     fi
   done
 }
