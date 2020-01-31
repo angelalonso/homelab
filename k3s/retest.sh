4089ca2b09c10143b5f14ff17095ad8062f846eb
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-01-31 16:38:29 +0100

    attaching nfs to IP

--- k3s/retest.sh
+++ k3s/retest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in ./apps/cns_fe/01_deployment.yaml ./apps/cns_be/01_deployment.yaml ./system/nfs-client.yaml ./system/nfs-server.yaml
+for i in ./apps/cns_fe/01_deployment.yaml ./apps/cns_be/01_deployment.yaml .apps/cns_resources/nfs-client-db-init.yaml ./system/nfs-client.yaml ./system/nfs-server.yaml
 do
   echo "### deleting $i"
   sudo k3s kubectl delete -f $i
@@ -8,7 +8,7 @@ done
 
 sleep 10
 
-for i in ./system/nfs-server.yaml ./system/nfs-client.yaml ./apps/cns_fe/01_deployment.yaml ./apps/cns_be/01_deployment.yaml
+for i in ./system/nfs-server.yaml ./system/nfs-client.yaml ./apps/cns_resources/nfs-client-db-init.yaml ./apps/cns_fe/01_deployment.yaml ./apps/cns_be/01_deployment.yaml
 do
   echo "### applying $i"
   sudo k3s kubectl apply -f $i
