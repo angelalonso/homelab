9ee99d27cc2cc0abcde0c813344c6c54c4f7eb18
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-01-31 16:11:16 +0100

    script to reapply stuff 5

--- k3s/retest.sh
+++ k3s/retest.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-sudo k3s kubectl delete -f ./apps/cns_fe/01_deployment.yaml
-sudo k3s kubectl delete -f ./apps/cns_be/01_deployment.yaml
-sudo k3s kubectl delete -f ./system/nfs-client.yaml
-sudo k3s kubectl delete -f ./system/nfs-server.yaml
+for i in ./apps/cns_fe/01_deployment.yaml ./apps/cns_be/01_deployment.yaml ./system/nfs-client.yaml ./system/nfs-server.yaml
+do
+  echo "### deleting $i"
+  sudo k3s kubectl delete -f $i
+done
 
 sleep 10
 
