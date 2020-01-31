1f594ccc1947cda5d23eb8c44c61d68f48cd89ee
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-01-31 16:06:38 +0100

    script to reapply stuff 4

--- k3s/retest.sh
+++ k3s/retest.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-k3s kubectl delete -f ./apps/cns_fe/01_deployment.yaml
-k3s kubectl delete -f ./apps/cns_be/01_deployment.yaml
-k3s kubectl delete -f ./system/nfs-client.yaml
-k3s kubectl delete -f ./system/nfs-server.yaml
+sudo k3s kubectl delete -f ./apps/cns_fe/01_deployment.yaml
+sudo k3s kubectl delete -f ./apps/cns_be/01_deployment.yaml
+sudo k3s kubectl delete -f ./system/nfs-client.yaml
+sudo k3s kubectl delete -f ./system/nfs-server.yaml
 
 sleep 10
 
-k3s kubectl apply -f ./system/nfs-server.yaml
-k3s kubectl apply -f ./system/nfs-client.yaml
-k3s kubectl apply -f ./apps/cns_fe/01_deployment.yaml
-k3s kubectl apply -f ./apps/cns_be/01_deployment.yaml
+sudo k3s kubectl apply -f ./system/nfs-server.yaml
+sudo k3s kubectl apply -f ./system/nfs-client.yaml
+sudo k3s kubectl apply -f ./apps/cns_fe/01_deployment.yaml
+sudo k3s kubectl apply -f ./apps/cns_be/01_deployment.yaml
