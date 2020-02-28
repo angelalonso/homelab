c2e274560254373bc6a0780e7394d51d6212a9a5
Ángel Alonso Fonseca <angel.fonseca@.deliveryhero.com>
2020-02-28 13:02:03 +0100

    adding functions to register and auth

--- security/scripts/local_u2f/local_u2f.py
+++ security/scripts/local_u2f/local_u2f.py
@@ -1,4 +1,6 @@
 import binascii
+import os
+import sys
 from pyu2f import model
 from pyu2f.u2f import GetLocalU2FInterface
 
@@ -13,6 +15,34 @@ def getHexlify(string):
     except:
         pass
     return binascii.hexlify(string)
+
+def getUnhexlify(data):
+    return binascii.unhexlify(data.replace("b'","").replace("'",""))
+
+
+def saveRegisteredDevices(loaded_keys, keys_file):
+    with open(keys_file, "w") as fh:
+        for key in loaded_keys:
+            fh.write(str(getHexlify(key)))
+    #os.chmod(keys_file, 0600)
+
+def loadRegisteredDevices(keys_file):
+    with open(keys_file) as f:
+        keys_list = f.readlines()
+    return keys_list
+
+def getConnectedDevice():
+    interface = None
+    message_shown = False
+    while interface is None:
+        try:
+            interface = GetLocalU2FInterface()
+        except:
+            if not message_shown:
+                print("Please connect your device")
+                message_shown = True
+    return interface
+
 
 def registerKey(registered_keys, interface):
     print("Touch your U2F Key to register it...")
@@ -36,11 +66,22 @@ def authenticateKey(interface, key):
     print("client_data=%s (%s)" % (client_data, type(client_data),))
 
 if __name__ == '__main__':
-    interface = GetLocalU2FInterface()
+    keys_file = 'keys.list'
+    interface = getConnectedDevice()
     registered_keys = []
-    key_handle, public_key = registerKey(registered_keys, interface)
-    print(key_handle)
-    print(public_key)
+    loaded_keys = []
+    if sys.argv[1] == 'register':
+        key_handle, public_key = registerKey(registered_keys, interface)
+        loaded_keys.append(key_handle)
+        saveRegisteredDevices(loaded_keys, keys_file)
+        print(key_handle)
+        print(public_key)
+    elif sys.argv[1] == 'authenticate':
+        loaded_keys = loadRegisteredDevices(keys_file)
+        print(loaded_keys)
+        key = model.RegisteredKey(getUnhexlify(loaded_keys[0]))
+        authenticateKey(interface, key)
+    else:
+        print("Wrong syntax")
+        sys.exit(2)
 
-    key = model.RegisteredKey(key_handle)
-    authenticateKey(interface, key)
