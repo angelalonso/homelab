4eb0205f00b209239ecbedd51e31611a66d90b63
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-05-17 15:29:37 +0200

    cleaning up funcs

--- infra/ctl/api/api.py
+++ infra/ctl/api/api.py
@@ -1,5 +1,6 @@
 import json
 import mysql.connector as mysql
+import yaml
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from os import getenv
@@ -15,6 +16,7 @@ def home():
     return "<h1>Available paths</h1> \
             <p>/host</p>"
 
+# TODO add one version of this for each object (not just /host)
 @app.route('/host', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def do_host():
     if request.method=='GET':
@@ -22,7 +24,7 @@ def do_host():
             if STORAGE == 'local':
                 result = DATA_MAIN['hosts']
             elif STORAGE == 'mysql':
-                result = db_select(db_connect(), DB_NAME, 'hosts', 'name, mac_address', "name LIKE '" + request.args["name"] + "'")
+                result = select_mysql(connectDb_mysql(), DB_NAME, 'hosts', 'name, mac_address', "name LIKE '" + request.args["name"] + "'")
         return jsonify(result)
     elif request.method=='PUT':
         host = {
@@ -39,10 +41,10 @@ def do_host():
             data_entry = {}
             data_entry['name'] = json_data['name']
             data_entry['mac_address'] = json_data['mac_address']
-            DATA_MAIN['hosts'].append(data_entry)
+            DATA_MAIN['host'].append(data_entry)
             result = DATA_MAIN
         elif STORAGE == 'mysql':
-            result = db_insert(db_connect(), DB_NAME, 'hosts', 'name, mac_address', "'" + request.json["name"] + "','" + request.json["mac_address"] + "'")
+            result = insert_mysql(connectDb_mysql(), DB_NAME, 'hosts', 'name, mac_address', "'" + request.json["name"] + "','" + request.json["mac_address"] + "'")
         return jsonify(result)
     elif request.method=='DELETE':
         if "name" in request.args:
@@ -52,15 +54,22 @@ def do_host():
                         DATA_MAIN['hosts'].remove(data_entry)
                 result = DATA_MAIN
             elif STORAGE == 'mysql':
-                result = db_delete(db_connect(), DB_NAME, 'hosts', "name LIKE '" + request.args["name"] + "'")
+                result = delete_mysql(connectDb_mysql(), DB_NAME, 'hosts', "name LIKE '" + request.args["name"] + "'")
         return jsonify(result)
 
 ''' Local Storage functions '''
 
+def createDb_local(obj_struct):
+    data_struct = {}
+    for entry in obj_struct:
+        data_struct[entry] = []
+    return data_struct
+
+
 ''' MYSQL functions '''
 
 # https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
-def db_connect():
+def connectDb_mysql():
     db_conn = mysql.connect(
         host = DB_HOST,
         user = DB_USER,
@@ -68,28 +77,28 @@ def db_connect():
     )
     return db_conn
 
-def db_show_dbs(db_conn):
+def showDbs_mysql(db_conn):
     cursor = db_conn.cursor()
     cursor.execute("SHOW DATABASES")
     records = cursor.fetchall()
     return records
 
-def db_create_db(db_conn, db_name):
+def createDb_mysql(db_conn, db_name):
     cursor = db_conn.cursor()
     cursor.execute("CREATE DATABASE " + db_name)
 
-def db_create_table(db_conn, db_name, table_name, structure):
+def createTable_mysql(db_conn, db_name, table_name, structure):
     cursor = db_conn.cursor()
     cursor.execute("CREATE TABLE " + db_name + "." + table_name + " " + structure)
 
-def db_select(db_conn, db_name, table_name, fields, where_params):
+def select_mysql(db_conn, db_name, table_name, fields, where_params):
     cursor = db_conn.cursor()
     sql_command = "SELECT " + fields + " FROM " + db_name + "." + table_name + " WHERE " + where_params + ";"
     cursor.execute(sql_command)
     records = cursor.fetchall()
     return records
 
-def db_insert(db_conn, db_name, table_name, fields, values):
+def insert_mysql(db_conn, db_name, table_name, fields, values):
     cursor = db_conn.cursor()
     sql_command = "INSERT INTO " + db_name + "." + table_name + "(" + fields + ") VALUES(" + values + ")"
     cursor.execute(sql_command)
@@ -97,10 +106,10 @@ def db_insert(db_conn, db_name, table_name, fields, values):
     result = cursor.rowcount
     return str(result)
 
-def db_update():
+def update_mysql():
     pass
 
-def db_delete(db_conn, db_name, table_name, where_params):
+def delete_mysql(db_conn, db_name, table_name, where_params):
     cursor = db_conn.cursor()
     sql_command = "DELETE FROM " + db_name + "." + table_name + " WHERE " + where_params + ";"
     cursor.execute(sql_command)
@@ -108,17 +117,34 @@ def db_delete(db_conn, db_name, table_name, where_params):
     result = cursor.rowcount
     return str(result)
 
+''' AUX Functions '''
+
+def loadObjectStruct(objects_file):
+    '''
+    Loads a yaml file into a data structure
+    '''
+    try:
+        with open(objects_file) as file:
+            objects = yaml.load(file, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        return None
+    return objects
+
 
 if __name__ == '__main__':
     load_dotenv()
+
+    OBJECTS_STRUCT_FILE = getenv("OBJECTS_STRUCT_FILE")
+    obj_struct = loadObjectStruct(OBJECTS_STRUCT_FILE)
+
     API_HOST = getenv("API_HOST")
     API_PORT = getenv("API_PORT")
     if API_HOST is None:
         API_HOST = "0.0.0.0"
+
     STORAGE = getenv("STORAGE")
     if STORAGE == 'local':
-        DATA_MAIN = {}
-        DATA_MAIN['hosts'] = []
+        DATA_MAIN = createDb_local(obj_struct)
     elif STORAGE == 'mysql':
         DB_HOST = getenv("DB_HOST")
         DB_PORT = getenv("DB_PORT")
